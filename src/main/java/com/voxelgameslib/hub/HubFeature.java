23210e8c3b4acb828df095b1eeffd99825ea8f06
MiniDigger <admin@minidigger.me>
2018-07-28 22:15:53 +0200

    delombok + formatting

--- src/main/java/com/voxelgameslib/hub/HubFeature.java
+++ src/main/java/com/voxelgameslib/hub/HubFeature.java
@@ -1,10 +1,10 @@
 package com.voxelgameslib.hub;
 
+import java.util.logging.Logger;
+
 import com.voxelgameslib.voxelgameslib.feature.AbstractFeature;
 
-import lombok.extern.java.Log;
-
-@Log
 public class HubFeature extends AbstractFeature {
 
+    private static final Logger log = Logger.getLogger(HubFeature.class.getName());
 }
